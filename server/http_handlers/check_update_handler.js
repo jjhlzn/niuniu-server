2502d8d8ec31dd14ef5c50ee8829bfddec2ac750
jjhlzn <jinjunhang@hotmail.com>
2017-12-10 14:34:31 +0800

    update

--- server/http_handlers/check_update_handler.js
+++ server/http_handlers/check_update_handler.js
@@ -9,10 +9,15 @@ var path = require('path');
 const logger = require('../utils/logger').logger(path.basename(__filename));
 
 let cur_android_verson = '3.0.4';
+let cur_iOS_version = '3.0.4';
 
 function isNeedUpdate(platform, version) {
   if (platform == 'Android') {
     if (version != cur_android_verson) {
+      return true;
+    }
+  } else if (platform == 'iOS') {
+    if (version != cur_iOS_version) {
       return true;
     }
   }
@@ -26,9 +31,11 @@ exports.handle = (req, res) => {
   let resp = { };
   
   resp.isNeedUpdate = isNeedUpdate(json.platform, json.version);
-  if (resp.isNeedUpdate) {
+  if (resp.isNeedUpdate && json.platform == 'Android') {
     resp.newVersion = cur_android_verson;
     resp.updateUrl = "http://www.hengdianworld.com/images/niuniu.apk";
+  } else if (resp.isNeedUpdate && json.platform == 'iOS') {
+    resp.newVersion = cur_iOS_version;
   }
 
   res.end(JSON.stringify(resp));;
