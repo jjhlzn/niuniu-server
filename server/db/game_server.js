d8dd73a58be98cbc880e69846a28a6f30ac75704
jjhlzn <jinjunhang@hotmail.com>
2017-12-19 10:20:01 +0800

    update
    测试多服务器

--- server/db/game_server.js
+++ server/db/game_server.js
@@ -1,9 +1,17 @@
 exports.makeServerUrl = () => {
   if (process.env.NODE_ENV == 'production') {
-    return "http://niu.yhkamani.com:3001"; // "http://192.168.1.114:3001"; //"
+
+    let servers = [
+      'http://niu.yhkamani.com/socket.io/',
+      'http://blog.jinjunhang.com:3001/socket.io/'
+    ];
+    
+    let index = Math.floor(Math.random() * 10000) % 2;
+
+    return servers[index]; // "http://192.168.1.114:3001"; //"
     //return "http://192.168.1.114:3001";
   } else {
-    return "http://192.168.31.175:3001"; // "http://192.168.1.114:3001"; //"
+    return "http://192.168.31.175:3001/socket.io/"; // "http://192.168.1.114:3001"; //"
     //return "http://192.168.1.117:3001"; 
   }
 } 
