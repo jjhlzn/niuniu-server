d208c20f022c1a7eb6d8fbce5c043f6da552633d
jjhlzn <jinjunhang@hotmail.com>
2020-05-19 15:58:43 +0800

    update

---
+++ http_handlers/index.js
@@ -0,0 +1,33 @@
+const checkuserInGameHandle = require('./check_user_in_game_handler').handle;
+const createRoomHandle = require('./create_room_handler').handle;
+const getRoomHandle = require('./get_room_handler').handle;
+const loginHandle = require('./login_handler').handle;
+const shareHandle = require('./share_handler').handle;
+const checkUpdateHandle = require('./check_update_handler').handle;
+const reportErrorHandle = require('./report_error_handler').handle;
+const getGameInfoHanlde = require('./get_game_info_handler').handle;
+const checkIosAuditVersionHanlde = require('./check_ios_audit_version_handler').handle;
+const auditLoginHandler = require('./audit_login_handler').handle;
+const express = require('express');
+
+ 
+
+module.exports = {
+    registerRoutes: function(app) {
+
+        app.use('/introduction.html', shareHandle);
+        app.use('/share', shareHandle);
+        app.use('/checkuseringame', checkuserInGameHandle);
+        app.use('/createroom', createRoomHandle);
+        app.use('/getroom', getRoomHandle);
+        app.use('/login', loginHandle);
+        app.use('/checkupdate', checkUpdateHandle);
+        app.use('/reporterror', reportErrorHandle);
+        app.use('/getgameinfo', getGameInfoHanlde);
+        app.use('/checkiosauditversion', checkIosAuditVersionHanlde);
+        app.use('/auditlogin', auditLoginHandler);
+        app.use(express.static('./clients'));
+        app.use(express.static('./clients/images'));
+        app.use(express.static('node_modules'));
+    }
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var registerNiuNiuHandlers = require('./server/niu/message_handlers/index').registerMessageHandlers
+var registerNiuNiuHandlers = require('./niu/message_handlers/index').registerMessageHandlers
+var registerRoutes = require('./http_handlers/index').registerRoutes
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,42 +10,16 @@ var http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 var path = require('path');
-const logger = require('./server/utils/logger').logger(path.basename(__filename));
+const logger = require('./utils/logger').logger(path.basename(__filename));
 
-//logger.debug("readyHandler = " + readyHandler)
-const checkuserInGameHandle = require('./server/http_handlers/check_user_in_game_handler').handle;
-const createRoomHandle = require('./server/http_handlers/create_room_handler').handle;
-const getRoomHandle = require('./server/http_handlers/get_room_handler').handle;
-const loginHandle = require('./server/http_handlers/login_handler').handle;
-const shareHandle = require('./server/http_handlers/share_handler').handle;
-const checkUpdateHandle = require('./server/http_handlers/check_update_handler').handle;
-const reportErrorHandle = require('./server/http_handlers/report_error_handler').handle;
-const getGameInfoHanlde = require('./server/http_handlers/get_game_info_handler').handle;
-const checkIosAuditVersionHanlde = require('./server/http_handlers/check_ios_audit_version_handler').handle;
-const auditLoginHandler = require('./server/http_handlers/audit_login_handler').handle;
-
-app.use('/introduction.html', shareHandle);
-app.use('/share', shareHandle);
-app.use('/checkuseringame', checkuserInGameHandle);
-app.use('/createroom', createRoomHandle);
-app.use('/getroom', getRoomHandle);
-app.use('/login', loginHandle);
-app.use('/checkupdate', checkUpdateHandle);
-app.use('/reporterror', reportErrorHandle);
-app.use('/getgameinfo', getGameInfoHanlde);
-app.use('/checkiosauditversion', checkIosAuditVersionHanlde);
-app.use('/auditlogin', auditLoginHandler);
-app.use(express.static('./clients'));
-app.use(express.static('./clients/images'));
-app.use(express.static('node_modules'));
-
-http.listen(3001);
-
+registerRoutes(app);
 io.on('connection', socket => {
   logger.debug("a new clent come in");
-
   //register 牛牛 message handlers 
   registerNiuNiuHandlers(io, socket)
 
 });
+
+http.listen(3001);
+
 
