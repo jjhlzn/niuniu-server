bb7c9bdfb327a1152028ff14dd5f7c531f98a3ce
jjhlzn <jinjunhang@hotmail.com>
2020-06-06 19:32:04 +0800

    update

--- 13shui/deck.js
+++ 13shui/deck.js
@@ -46,6 +46,22 @@ function deal(game) {
  */
 function compareCards(cardsResult) {
   let playerIds = _.keys(cardsResult)
+  var scoreMatrix = []
+  for(var i = 0; i < playerIds.length; i++) {
+    var score = []
+    for(var j = 0; j < playerIds.length; j++) {
+      score.push({score: 0, isAllWin: false, isAllLose: false})
+    }
+    scoreMatrix.push(score)
+  } 
+
+  for(var i = 0; i < playerIds.length; i++) {
+    for(var j = i + 1; j < playerIds.length; j++) {
+      var result1 = cardsResult[playerIds[i]]
+      var result2 = cardsResult[playerIds[j]]
+      compareCards1(result1, result2)
+    }
+  }
   let scoreResults = {}
   playerIds.forEach(playerId => {
     scoreResults[playerId] = 0
@@ -57,33 +73,63 @@ function compareCards1(result1, result2) {
   if (result1.specialCardType && result2.specialCardType) {
     var score = getSpecialCardTypeDaos(result1.specialCardType) 
             - getSpecialCardTypeDaos(result2.specialCardType)
-    return [score, -score]
+    return {score: score, isAllWin: false, isAllLose: false}
   }
 
   if (result1.specialCardType) {
     var score = getSpecialCardTypeDaos(result1.specialCardType)
-    return [score, -score]
+    return{score: score, isAllWin: false, isAllLose: false}
   }
 
   if (result2.specialCardType) {
     var score= getSpecialCardTypeDaos(result2.specialCardType)
-    return [-score, score]
+    return {score: -score, isAllWin: false, isAllLose: false}
   }
 
   var score = 0
+  var winDaos = 0
   for(var i = 0; i < 3; i++) {
     var score1 = compareDao(i, result1.cardsResult[i], result2.cardsResult[i])
+    if (score1 > 0) {
+      winDaos++
+    }
     score += score1
   }
-  return [score, -score]
+  return {score: score, isAllWin: winDaos == 3, isAllLose: winDaos == 0}
 }
 
+/*
+public static string SPECIAL_FLUSH = "special_flush"; //三同花
+public static string SPECIAL_STRAIGHT = "special_straight"; //三顺子
+public static string SPECIAL_PAIR = "special_pair";    //6对半
+public static string SPECIAL_PAIR_WITH_THREE = "special_pair_with_three"; //5对冲3
+public static string SPECIAL_LONG_STRAIGHT = "special_long_straight"; //一条龙
+public static string SPECIAL_TRIPLE = "special_three"; //4个3条
+public static string SPECIAL_ALL_THE_SAME_COLOR = "special_all_the_same_color"; 
+*/
 function  getSpecialCardTypeDaos(specialCardType) {
-  return 0
+  //TODO: 检查是不是特殊牌型
+  switch (specialCardType) {
+    case 'special_flush':
+    case 'special_straight':
+    case 'special_pair':
+      return 6;
+    case 'special_pair_with_three':
+      return 9;
+    case 'special_long_straight':
+      return 26;
+    case 'special_three':
+      return 32;
+    case 'special_all_the_same_color':
+      return 26
+  }
+  throw 'not supported special card type: ' + specialCardType
 }
 
-function compareDao(daoNo, cards1, cards) {
-  return 0
+function compareDao(daoNo, cards1, cards2) {
+  var cardType1 = getCardType(cards1)
+  var cardType2 = getCardType(cards2)
+  return cardType1.compare(daoNo, cardType2) 
 }
 
 
--- 13shui/models/cardType.js
+++ 13shui/models/cardType.js
@@ -1,8 +1,119 @@
+/*
+public static string HIGH = "high";
+public static string ONE_PAIR = "onepair";
+public static string TWO_PAIR = "twopair";
+public static string TRIPLE = "triple";
+public static string STRAIGHT = "straight";
+public static string FLUSH = "flush";
+public static string FULLHOURSE = "fullhourse";
+public static string QUARD = "quard";
+public static string FLUSHSTRAIGHT = "flushstraight";
+ */ 
+class Card {
+    point
+    type
+    constructor(value) {
+        this.point = value[0]
+        this.type = value[1]
+    }
+
+    pointValue() {
+        switch(this.point) {
+            case 'T':
+                return 10
+            case 'J':
+                return 11
+            case 'Q':
+                return 12
+            case 'K': 
+                return 13
+            case 'A':
+                return 14
+            default:
+                return parseInt(this.point)
+        }
+    }
+}
 
 class OnePairCardType {
-    
+    identify(cards) {
+        cards = cards.map(card => new Card(card))
+        var points = cards.map(card => card.point)
+        var set = new Set(point)
+        return set.size < cards.length
+    }
+
+    getMainPoint() {
+        return 1
+    }
+
+    sort() {
+
+    }
+
+    getPoint() {
+
+    }
+
+    getDaos(daoNo) {
+        return 1
+    }
 }
 
 class TwoPairCardType {
     
 }
+
+class TripleCardType {
+
+}
+
+class StraightCardType {
+
+}
+
+class FLushCardType {
+
+}
+
+class FullHourseCardType {
+
+}
+
+class QuardCardType {
+
+}
+
+class FlushStraightCardType {
+
+}
+
+
+function getCardType(cards) {
+
+}
+
+function compare(cardType1, cardType2, daoNo) {
+    var diff = cardType1.getMainPoint() - cardType2.getMainPoint()
+    
+    if (diff > 0) {
+        return cardType1.getDaos(daoNo)
+    } else if (diff < 0) {
+        return cardType2.getDaos(daoNo)
+    } else {
+        diff = cardType1.getPoint() - cardType2.getPoint()
+        if (diff > 0) {
+            return cardType1.getDaos(daoNo)
+        } else if (diff < 0) {
+            return cardType2.getDaos(daoNo)
+        } else {
+            diff = cardType1.getPoint() - cardType2.getPoint()
+            return 0
+        }
+    }
+}
+
+var card = new Card('Ts')
+console.log(card.type)
+console.log(card.point)
+console.log(card.pointValue())
